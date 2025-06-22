63a5ebf89e26db70d0691fc92aa1a261ce874049
Quentin Chen <quentinchen2164@example.com>
2025-06-22 04:48:55 -0700

    feat: add filter component, header bar, and climb card container

--- client/src/pages/Climbs.jsx
+++ client/src/pages/Climbs.jsx
@@ -1,10 +1,34 @@
 import './Climbs.css'
+import '../components/SidebarFilter'
+import SidebarFilter from '../components/SidebarFilter';
 
 
 export default function Climbs() {
     return (
-        <div>
-            <h2>Your Climbs</h2>
+        <div className='climbs-page'>
+            <div className='climbs-main'>
+                <div className='climbs-header'>
+                    <h2>Your Climbs</h2>
+                    <div className='tags-container'>
+                        <label>VB-V1</label>
+                        <div className='green-tag-bar' />
+                        <label>V2-V4</label>
+                        <div className='red-tag-bar' />
+                        <label>V5-V7</label>
+                        <div className='blue-tag-bar' />
+                        <label>V8-V9</label>
+                        <div className='purple-tag-bar' />
+                        <label>V10+</label>
+                        <div className='black-tag-bar' />
+                    </div>
+                    <div className='climbs-controls'>
+                        <button>Add Climb</button>
+                        <button>Sort</button>
+                    </div>
+                </div>
+                <SidebarFilter />
+                <div className='climbs-grid'></div>
+            </div>
         </div>
     );
 };
