e4273febc5b44c9ddbd986e80fca63c4f449439b
Quentin Chen <quentinchen2164@example.com>
2025-06-30 19:24:18 -0700

    Navigate to home page after sign in through Google

--- client/src/components/GoogleSignIn.jsx
+++ client/src/components/GoogleSignIn.jsx
@@ -1,14 +1,18 @@
 import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from '../firebase/firebaseConfig';
 import './GoogleSignIn.css'
+import { useNavigate } from 'react-router-dom';
 
 export default function GoogleSignIn() {
+    const navigate = useNavigate();
+
     const handleGoogleSignIn = async () => {
         try {
             const result = await signInWithPopup(auth, googleProvider);
             const user = result.user;
             alert(`Welcome ${user.displayName}!`);
             console.log(user);
+            navigate('/home')
         } catch (error) {
             console.log('Google Sign-In Error', error);
             alert(error.message);
