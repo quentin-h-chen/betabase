959607a6f4e0c8380b274ee22063ae10b6fe43f7
Quentin Chen <quentinchen2164@example.com>
2025-07-06 16:37:44 -0700

    Created layout for homepage, Added color when hovering links on navbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,18 @@ import './Home.css'
 
 export default function Home() {
     return (
-        <div>
-            <h2>Welcome to Betabase</h2>
-            <p>Track your indoor bouldering projects with ease.</p>
+        <div className='home-page'>
+            <div className='home-grid'>
+                <div className='container container1'>
+                    <h2>Welcome to Betabase</h2>
+                </div>
+                <div className='container container2'>
+                    <h2>Your Data</h2>
+                </div>
+                <div className='container container3'>
+                    <h2>Recent Climbs</h2>
+                </div>
+            </div>
         </div>
     );
 };
